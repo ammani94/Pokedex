1a04f8b0e3adf7a36ca45023bcbdfbd255ab1bf6
Idir Ammani <Idir@mbp-de-idir-3.home>
2019-06-06 15:48:48 +0200

    second  commit

---
+++ src/app/pokemon-detail-view/pokemon-detail-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-detail-view',
+  templateUrl: './pokemon-detail-view.component.html',
+  styleUrls: ['./pokemon-detail-view.component.scss']
+})
+export class PokemonDetailViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
